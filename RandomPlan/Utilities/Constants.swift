d9c072c1a742abf0ef0d2252f3ee1f0e087b35c7
Alejandro Terrazas <alex.terrazas.z@gmail.com>
2020-09-07 20:17:04 -0500

    Remove KEYS

--- RandomPlan/Utilities/Constants.swift
+++ RandomPlan/Utilities/Constants.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Constant {
-    static let YT_API_KEY           = "AIzaSyARSBjhhYnxKI0te6y9XyBNQ4_wbGZmKnY"
-    static let OMDB_API_KEY         = "9cc5a179"
-    static let THEMOVIEDB_API_KEY   = "a8e53f1594c1ba9c7b98e0869bb2c86f"
+    static let YT_API_KEY           = ""
+    static let OMDB_API_KEY         = ""
+    static let THEMOVIEDB_API_KEY   = ""
 }
